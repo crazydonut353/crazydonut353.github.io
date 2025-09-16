8a99164f41bb4e9ebfc83c0bf7fbf428463227d4
crazydonut353 <117462853+crazydonut353@users.noreply.github.com>
2025-09-16 12:28:38 -0500

    Update script.js
    added physics

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 class Vec2 {
-  constructor() {
-    this.x = 0;
-    this.y = 0;
-  }
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -10,15 +6,9 @@ class Vec2 {
 }
 
 class PhysicsBase {
-  constructor() {
-    this.force = new Vec2();
-    this.velocity = new Vec2();
-    this.position = new Vec2();
-  }
-
   constructor(x, y) {
-    this.force = new Vec2();
-    this.velocity = new Vec2();
+    this.force = new Vec2(0,0);
+    this.velocity = new Vec2(0,0);
     this.position = new Vec2(x, y);
   }
 
@@ -41,8 +31,12 @@ class SpringJoint {
   }
 
   update() {
-    this.tail.force.x = -this.k * (this.tail.position.x - this.anchor.position.x);
-    this.tail.force.y = -this.k * (this.tail.position.y - this.anchor.position.y);
+    this.tail.force.x = (-this.k * (this.tail.position.x - this.anchor.x));
+    this.tail.force.y = (-this.k * (this.tail.position.y - this.anchor.y));
+    
+    // something like this models a form of damping... maybe
+    this.tail.force.x += this.tail.velocity.x * this.d;
+    this.tail.force.y += this.tail.velocity.y * this.d;
 
     this.tail.update();
   }
@@ -51,7 +45,7 @@ class SpringJoint {
 const c = document.getElementById("c");
 const ctx = c.getContext("2d");
 const g = .1;
-let spring = new SpringJoint(new Vec2(10, 10), new PhysicsBase(10, 50), .1, .4);
+let spring = new SpringJoint(new Vec2(10, 10), new PhysicsBase(10, 50), .1, -.1);
 
 function resize() {
   c.width = window.innerWidth + 8;
@@ -67,12 +61,19 @@ function drawLine(x1, y1, x2, y2) {
 }
 
 async function init() {
+  resize();
+  
+  document.addEventListener("mousemove", (e) => {spring.anchor.x=e.clientX+8;spring.anchor.y=e.clientY+8;})
+    
   loop();
 }
 
 function loop() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0,0,c.width,c.height)
+
   spring.update();
-  drawLine(spring.anchor.position.x,spring.anchor.position.y,spring.tail.position.x,spring.tail.position.x);
+  drawLine(spring.anchor.x,spring.anchor.y,spring.tail.position.x,spring.tail.position.y);
   
   requestAnimationFrame(loop);
 }
