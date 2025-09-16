06afbab33fb51c61bfec74bc6d396eb6b642e092
crazydonut353 <117462853+crazydonut353@users.noreply.github.com>
2025-09-16 10:35:33 -0500

    Update script.js
    yup

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
 const c = document.getElementById("c");
 const ctx = c.getContext("2d");
+const g = .1;
 
-c.width = window.innerWidth;
-c.height = window.innerHeight;
+function resize() {
+  c.width = window.innerWidth + 8;
+  c.height = window.innerHeight + 8;
+}
 
-ctx.beginPath();
-ctx.moveTo(20, 20);
-ctx.lineTo(20, 100);
-ctx.lineTo(70, 100);
-ctx.strokeStyle = "red";
-ctx.stroke();
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = "red";
+  ctx.stroke();
+}
+
+class Vec2 {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+  }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class PhysicsBase {
+  constructor() {
+    this.force = new Vec2();
+    this.velocity = new Vec2();
+    this.position = new Vec2();
+  }
+
+  update() {
+    //add collision soon
+    this.velocity.x += this.force.x;
+    this.velocity.y += this.force.y;
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+}
+
+class SpringJoint {
+  constructor(anchor, tail, k, d) {
+    this.anchor = anchor;
+    this.tail = tail;
+    this.k = k;
+    this.d = d;
+  }
+
+  update() {
+    this.tail.force.x = -this.k * (this.tail.position.x - this.anchor.position.x);
+    this.tail.force.y = -this.k * (this.tail.position.y - this.anchor.position.y);
+
+    this.tail.velocity.x += this.tail
+  }
+}
+
+async function init() {
+  loop();
+}
+
+function loop() {
+  requestAnimationFrame(loop);
+}
+
+init();
